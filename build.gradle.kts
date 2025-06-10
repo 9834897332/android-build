b0ae1d72bcd9263b9228664b8b5c67d596d53c49
kartik kohapare <kartikkohapare527@gmail.com>
2025-06-11 02:19:38 +0530

    Moved Android project to root for CI/CD and added base Android files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,4 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.3.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-    }
-}
-
-allprojects {
+pluginManagement {
     repositories {
         google()
         mavenCentral()
